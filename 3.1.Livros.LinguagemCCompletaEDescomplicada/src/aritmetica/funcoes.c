b9e7cdcc907b26767157ec9422c6264ef7903573
Sóstenes Gonçalves de Souza <sostenes198br@hotmail.com>
2023-09-11 22:39:51 -0300

    Estudos C

---
+++ 3.1.Livros.LinguagemCCompletaEDescomplicada/src/aritmetica/funcoes.c
@@ -0,0 +1,22 @@
+//
+// Created by soste on 11/09/2023.
+//
+
+#include "funcoes.h"
+
+int soma(int a, int b) {
+    return a + b;
+}
+
+int subtracao(int a, int b) {
+    return a - b;
+}
+
+int produto(int a, int b) {
+    return a * b;
+}
+
+int divisao(int a, int b) {
+    return a / b;
+}
+
---
+++ 3.1.Livros.LinguagemCCompletaEDescomplicada/src/aritmetica/funcoes.h
@@ -0,0 +1,11 @@
+//
+// Created by soste on 11/09/2023.
+//
+
+int soma(int a, int b);
+
+int subtracao(int a, int b);
+
+int produto(int a, int b);
+
+int divisao(int a, int b);
